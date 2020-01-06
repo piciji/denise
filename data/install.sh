2f04e54d7ce385ca4b23c1ced17e43804464eb2a
PiCiJi <piciji@users.sourceforge.net>
2020-01-06 11:58:01 +0100

    testing

--- data/install.sh
+++ data/install.sh
@@ -8,7 +8,7 @@ mkdir -p $prefix/share/applications/
 mkdir -p $prefix/denise/translation/
 mkdir -p $prefix/denise/data/
 mkdir -p $prefix/denise/fonts/
-mkdir -p $(prefix)/$(loname)/shader/
+mkdir -p $prefix/denise/shader/
 
 install -D -m 755 Denise $prefix/bin/Denise
 install -D -m 644 denise.png $prefix/share/icons/denise.png
@@ -16,4 +16,4 @@ install -D -m 644 denise.desktop $prefix/share/applications/denise.desktop
 install -D -m 644 translation/* $prefix/denise/translation
 install -D -m 644 data/* $prefix/denise/data
 install -D -m 644 fonts/* $prefix/denise/fonts
-install -D -m 644 data/shader/* $(prefix)/$(loname)/shader
+install -D -m 644 shader/* $prefix/denise/shader
