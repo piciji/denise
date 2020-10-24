5670059834934a81a880590f481c7503c5c28f3d
PiCiJi <piciji@users.sourceforge.net>
2020-10-24 15:14:45 +0200

    update c64 core revision

--- emulation/libc64/interface.cpp
+++ emulation/libc64/interface.cpp
@@ -20,7 +20,7 @@
 
 namespace LIBC64 {
 
-const std::string Interface::Version = "10896";
+const std::string Interface::Version = "109";
     
 Interface::Interface() : Emulator::Interface( "C64" ) {        
     
