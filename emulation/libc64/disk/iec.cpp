b0f2c4bf41153b0793c756e8021a011826a122bf
PiCiJi <piciji@users.sourceforge.net>
2020-10-25 13:34:43 +0100

    version 1.0.9 is ready

--- emulation/libc64/disk/iec.cpp
+++ emulation/libc64/disk/iec.cpp
@@ -368,10 +368,10 @@ auto IecBus::randomizeRpm() -> void {
 auto IecBus::attach( Emulator::Interface::Media* media, uint8_t* data, unsigned size ) -> void {
     
     if (system->diskSilence.idle) {
-        system->diskSilence.idle = false;
-        system->diskSilence.idleFrames = 0;
+        system->diskSilence.idle = false;        
         resetTicks();
     }
+    system->diskSilence.idleFrames = 0;
     
     drives[ media->id ]->attach( media, data, size );   
 }
