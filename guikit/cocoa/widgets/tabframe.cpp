62ffb7d545a9ee9887337146e60fa3fb734920db
PiCiJi <piciji@users.sourceforge.net>
2020-11-25 19:48:15 +0100

    test buildsystem

--- guikit/cocoa/widgets/tabframe.cpp
+++ guikit/cocoa/widgets/tabframe.cpp
@@ -42,8 +42,6 @@
 
 -(void) drawLabel:(BOOL)shouldTruncateLabel inRect:(NSRect)tabRect {
     
-    bool _mojaveMin = GUIKIT::hasMinimumVersion(10, 14);
-    
     signed selection = [cocoaTabFrame indexOfTabViewItem:self];
     if(selection >= 0) {
         if(p->cocoaImages.size() > selection) {
@@ -52,7 +50,7 @@
                 unsigned iconSize = GUIKIT::pFont::size([cocoaTabFrame font], " ").height;
             
                 [[NSGraphicsContext currentContext] saveGraphicsState];
-                NSRect targetRect = NSMakeRect(tabRect.origin.x, tabRect.origin.y + (_mojaveMin ? 1 : 1), iconSize, iconSize);
+                NSRect targetRect = NSMakeRect(tabRect.origin.x, tabRect.origin.y + 1, iconSize, iconSize);
                 [image drawInRect:targetRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0 respectFlipped:YES hints:nil];
                 [[NSGraphicsContext currentContext] restoreGraphicsState];
             
@@ -61,7 +59,7 @@
             }
         }
     }
-    tabRect.origin.y += _mojaveMin ? 1 : 1;
+    tabRect.origin.y += 1;
     [super drawLabel:shouldTruncateLabel inRect:tabRect];
 }
 @end
