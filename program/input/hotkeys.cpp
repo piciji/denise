387e4a7b1c1ff2134cd63715f224ab352625bf65
PiCiJi <piciji@users.sourceforge.net>
2022-01-01 20:19:53 +0100

    improve readability of sync status

--- program/input/hotkeys.cpp
+++ program/input/hotkeys.cpp
@@ -234,11 +234,15 @@ auto InputManager::fireHotkey(Emulator::Interface* emulator, Hotkey::Id id) -> v
             openMenu( emulator, id );
             break;
         case Hotkey::Id::SyncStatus: {
-            std::string _str = videoDriver->hasSynchronized() ? "Vsync    " : "";
-            _str += videoDriver->hasThreaded() ? "Threaded    " : "";
-            _str += VideoManager::frameRenderTrigger > 1 ? std::to_string(VideoManager::frameRenderTrigger) + "x    " : "";
-            _str += audioDriver->hasSynchronized() ? "Audiosync    " : "";
-            _str += audioManager->dynamicRateControl ? "DRC    " : "";
+            std::string _str = videoDriver->hasThreaded() ? "Threaded  " : "";
+
+            if (videoDriver->hasSynchronized()) {
+                _str += "Vsync";
+                _str += VideoManager::frameRenderTrigger > 1 ? "(" + std::to_string(VideoManager::frameRenderTrigger) + "x)  " : "  ";
+            }
+
+            _str += audioDriver->hasSynchronized() ? "Audiosync  " : "";
+            _str += audioManager->dynamicRateControl ? "DRC  " : "";
             float monitorFrequency = GUIKIT::Monitor::getCurrentRefreshRate();
             _str += GUIKIT::String::convertDoubleToString(monitorFrequency, 3 );
             statusHandler->setMessage(_str, 6);
