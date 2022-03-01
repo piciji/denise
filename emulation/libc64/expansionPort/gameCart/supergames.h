3e122519cdb3fa14f95cbde07050f65623d0a0bc
piciji <piciji@users.sourceforge.net>
2022-03-01 21:52:01 +0100

    autostart doesn't eject mafiosino fastloader

--- emulation/libc64/expansionPort/gameCart/supergames.h
+++ emulation/libc64/expansionPort/gameCart/supergames.h
@@ -9,7 +9,12 @@ struct SuperGames : GameCart {
         writeProtect = false;
     }       
 
-    bool writeProtect = false; 	
+    bool writeProtect = false;
+	bool mafiosino = false; // fastloader
+	
+	auto isBootable( ) -> bool {
+		return !mafiosino;
+	}
 	
     auto writeIo2( uint16_t addr, uint8_t value ) -> void {
         
@@ -37,6 +42,14 @@ struct SuperGames : GameCart {
         cRomH = getChip(0);
 
 		writeProtect = false;
+		
+		uint8_t mafioPattern[] = { 0x4d, 0x41, 0x46, 0x49, 0x4f };
+		uint8_t* mafioPatternLast = mafioPattern + sizeof(mafioPattern);
+		uint8_t* dataLast = data + std::min<unsigned>(2048, size);
+
+		uint8_t* result = std::search(data, dataLast, mafioPattern, mafioPatternLast);
+		
+		mafiosino = result != dataLast;
     }
 
     auto assumeChips( ) -> void {
@@ -48,7 +61,9 @@ struct SuperGames : GameCart {
     
         Cart::serializeStep2( s );
 
-        s.integer(writeProtect);   
+        s.integer(writeProtect);
+		
+		s.integer(mafiosino);
     }
 };
     
