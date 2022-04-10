77b6c66cb9ec7858a029e34a13067a5e80a63170
PiCiJi <piciji@users.sourceforge.net>
2022-04-10 21:11:48 +0200

    update cocoa listview row adjustments for custom font in dependance of minimum target version

--- guikit/cocoa/widgets/listview.cpp
+++ guikit/cocoa/widgets/listview.cpp
@@ -198,20 +198,44 @@
         // this is a hack to completly remove row spacing
         unsigned fontSize = GUIKIT::pFont::getSizeFromString( listView->font() );
         
-        if (fontSize == 6 || fontSize == 7 || fontSize == 11) {
-            listView->p.fontAdjust.rowHeight = -3;
-            listView->p.fontAdjust.yOffset = -2;
-            listView->p.fontAdjust.height = 2;
+        if (__MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_13) { // build for Mojave and above
+            listView->p.fontAdjust.rowHeight = 0;
+            listView->p.fontAdjust.yOffset = 0;
+            listView->p.fontAdjust.height = 0;
             
-        } else if ( fontSize == 8 || fontSize == 9 || fontSize == 12 || fontSize == 13 || fontSize == 14 ) {
-            listView->p.fontAdjust.rowHeight = -5;
-            listView->p.fontAdjust.yOffset = -4;
-            listView->p.fontAdjust.height = 4;
-            
-        } else if ( fontSize == 10 ) {
-            listView->p.fontAdjust.rowHeight = -4;
-            listView->p.fontAdjust.yOffset = -3;
-            listView->p.fontAdjust.height = 3;
+            if (fontSize == 8 || fontSize == 9) {
+                listView->p.fontAdjust.rowHeight = -1;
+                listView->p.fontAdjust.yOffset = 0;
+                listView->p.fontAdjust.height = 1;
+            } else if (fontSize == 10) {
+                listView->p.fontAdjust.rowHeight = 0;
+                listView->p.fontAdjust.yOffset = 1;
+                listView->p.fontAdjust.height = 0;
+            } else if (fontSize == 11) {
+                listView->p.fontAdjust.rowHeight = 0;
+                listView->p.fontAdjust.yOffset = 1;
+                listView->p.fontAdjust.height = 1;
+            } else if (fontSize == 13 || fontSize == 14) {
+                listView->p.fontAdjust.rowHeight = 0;
+                listView->p.fontAdjust.yOffset = 1;
+                listView->p.fontAdjust.height = 0;
+            }
+        } else {
+            if (fontSize == 6 || fontSize == 7 || fontSize == 11) {
+                listView->p.fontAdjust.rowHeight = -3;
+                listView->p.fontAdjust.yOffset = -2;
+                listView->p.fontAdjust.height = 2;
+                
+            } else if ( fontSize == 8 || fontSize == 9 || fontSize == 12 || fontSize == 13 || fontSize == 14 ) {
+                listView->p.fontAdjust.rowHeight = -5;
+                listView->p.fontAdjust.yOffset = -4;
+                listView->p.fontAdjust.height = 4;
+                
+            } else if ( fontSize == 10 ) {
+                listView->p.fontAdjust.rowHeight = -4;
+                listView->p.fontAdjust.yOffset = -3;
+                listView->p.fontAdjust.height = 3;
+            }
         }
     }
 
