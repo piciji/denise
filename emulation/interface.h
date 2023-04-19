bf4bb492b2446ffc763224c63bed01cf9b582941
PiCiJi <piciji@users.sourceforge.net>
2023-04-20 00:03:27 +0200

    fix

--- emulation/interface.h
+++ emulation/interface.h
@@ -435,7 +435,11 @@ struct Interface {
         if (asHex) {
             out = "0x";
             char hex[8];
+#ifdef _MSC_VER
             sprintf_s( hex, "%x", data );
+#else
+            sprintf( hex, "%x", data );
+#endif
             out += (std::string)hex;
         }
         bind->log(out, newLine);
