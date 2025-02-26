d63c27b5c15f37ed930fbf2a0c87d3e4c66ec823
PiCiJi <piciji@users.sourceforge.net>
2025-02-26 20:02:56 +0100

    bugfix

--- program/view/status.cpp
+++ program/view/status.cpp
@@ -240,7 +240,7 @@ auto StatusHandler::setExpansionClick() -> void {
     auto expansion = activeEmulator->getExpansion();
 
     if (dynamic_cast<LIBC64::Interface*>(activeEmulator) &&
-        (expansion->id == LIBC64::Interface::ExpansionId::ExpansionIdSuperCpu) || (expansion->id == LIBC64::Interface::ExpansionId::ExpansionIdSuperCpuReu)) {
+        ((expansion->id == LIBC64::Interface::ExpansionId::ExpansionIdSuperCpu) || (expansion->id == LIBC64::Interface::ExpansionId::ExpansionIdSuperCpuReu))) {
         emuThread->lock();
         bool state = activeEmulator->getExpansionJumper(expansion->mediaGroup->selected, 0);
         activeEmulator->setExpansionJumper(expansion->mediaGroup->selected, 0, !state);
